4e1735fc43782f7ead5a705504fd195b088b5673
Michael Mason <TeaUponTweed@gmail.com>
2022-07-04 14:31:10 -0600

    finish decitab

--- background.js
+++ background.js
@@ -1,26 +1,18 @@
 chrome.tabs.onCreated.addListener((newTab) => {
-  // console.log(newTab);
   chrome.tabs.query({windowType:'normal'}, function(tabs) {
-    // windowTabCounts = {}
     currentWindowTabCount = 0;
     tabs.map((tab) => {
-      // console.log(tab.windowId)
-      // if (!(tab.windowId in windowTabCounts)) {
-      //   windowTabCounts[tab.windowId] = 0
-      // }
-      // windowTabCounts[tab.windowId] += 1;
       if (tab.windowId == newTab.windowId) {
         currentWindowTabCount += 1
       }
     })
-    // console.log(currentWindowTabCount);
     if (currentWindowTabCount > 10) {
       chrome.tabs.remove(newTab.id, () => {
         console.log('ere');
         chrome.notifications.create(
         "test", {
             type: 'basic',
-            iconUrl: 'images/icon.png',
+            iconUrl: 'images/10.png',
             title: 'Too Many Tabs!',
             message: 'Close some. For all our sanity.',
             priority: 2,
